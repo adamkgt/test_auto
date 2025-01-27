e9f48bbd646931a649b9070ca73f2832dcda592b
Adam <adamkgt@gmail.com>
2025-01-27 20:06:09 +0100

    add_first_tests_and_update_environment

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,24 @@
 const { defineConfig } = require("cypress");
-
 module.exports = defineConfig({
+
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
   },
+  
 });
+
+// cypress.config.js
+module.exports = {
+  e2e: {
+    integrationFolder: "test_auto",  // ścieżka do folderu z testami
+  },
+}
+
+// cypress.config.js
+module.exports = {
+  e2e: {
+    specPattern: "cypress/my_tests/**/*.js", // Ścieżka do testów
+  },
+}
