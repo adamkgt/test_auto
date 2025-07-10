30af693ce6c3774f470ad76bd527a9e4cbcec09f
Adam <adamkgt@gmail.com>
2025-07-10 21:42:26 +0200

    Update tests

--- cypress/my_tests/fake_store_page.cy.js
+++ cypress/my_tests/fake_store_page.cy.js
@@ -19,9 +19,11 @@ describe('User registration', () => {
     cy.get('#reg_email').type(dataLogin.username); 
     cy.get('#reg_password').type(dataLogin.wrongPassword);
     cy.wait(5000); 
-    cy.get('#customer_login > div.u-column2.col-2 > form > p:nth-child(5) > button').should('be.disabled'); 
-    cy.url().should('include', '/moje-konto'); 
-    cy.get('#password_strength').should('contain.text', 'Bardzo słabe - Proszę wpisać mocniejsze hasło.'); 
+    cy.get('button.woocommerce-Button.woocommerce-button.button.woocommerce-form-register__submit').screenshot('before-disabled-check').then($btn => {
+    console.log('disabled:', $btn.prop('disabled'))
+  }).should('be.disabled')
+    cy.url().should('include', '/moje-konto');
+    cy.get('#password_strength').should('contain.text', 'Bardzo słabe - Proszę wpisać mocniejsze hasło.');
     })
    })
 
