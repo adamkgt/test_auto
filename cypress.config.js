89b0363bdda6a116d27595641a3e0d6bd9e360f3
Adam <adamkgt@gmail.com>
2025-07-10 20:44:15 +0200

    Add files

--- cypress.config.js
+++ cypress.config.js
@@ -1,34 +1,10 @@
 const { defineConfig } = require("cypress");
-const createEsbuildPlugin = require("@badeball/cypress-cucumber-preprocessor/esbuild").createEsbuildPlugin;
 
 module.exports = defineConfig({
   e2e: {
+    specPattern: "cypress/my_tests/**/*.cy.{js,ts}", // ścieżka do testów
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      // opcjonalne eventy/pluginy
     },
   },
-});
-
-// cypress.config.js
-module.exports = {
-  e2e: {
-    integrationFolder: "test_auto",  // ścieżka do folderu z testami
-  },
-}
-
-// cypress.config.js
-module.exports = {
-  e2e: {
-    specPattern: "cypress/my_tests/**/*.js", // Ścieżka do testów
-  },
-}
-
-module.exports = defineConfig({
-  e2e: {
-    setupNodeEvents(on, config) {
-      require("@badeball/cypress-cucumber-preprocessor").addCucumberPreprocessorPlugin(on, config);
-      return config;
-    },
-    specPattern: "cypress/my_tests/**/*.feature"
-  }
 });
